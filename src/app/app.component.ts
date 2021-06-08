55f2a9570daa84e237ad38641e589514c80d7698
Perry Brandiezs <pbrandiezs@gmail.com>
2021-06-08 12:27:44 -0500

    feat: Implemented Project Name field on form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectData': new FormGroup({
+        'projectName': new FormControl(null, [Validators.required])
+      })
+    });
+
+    // this.projectForm.statusChanges.subscribe(
+    //   (status) => console.log(status)
+    // );
+    this.projectForm.setValue({
+      'projectData': {
+        'projectName': 'abc'
+      }
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
 }
